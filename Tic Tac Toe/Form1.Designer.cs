c2e317f0457637126931156b819aebc4b1388aca
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2024-02-29 23:02:13 +0800

    Add project files.

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(50, 114);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 75);
+            this.b1.TabIndex = 0;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(155, 114);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(75, 75);
+            this.b2.TabIndex = 1;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(260, 114);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(75, 75);
+            this.b3.TabIndex = 2;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(50, 220);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(75, 75);
+            this.b4.TabIndex = 3;
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(155, 220);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(75, 75);
+            this.b5.TabIndex = 4;
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(260, 220);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(75, 75);
+            this.b6.TabIndex = 5;
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(50, 326);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(75, 75);
+            this.b7.TabIndex = 6;
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(155, 326);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(75, 75);
+            this.b8.TabIndex = 7;
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(260, 326);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(75, 75);
+            this.b9.TabIndex = 8;
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestart.ForeColor = System.Drawing.Color.Black;
+            this.btnRestart.Location = new System.Drawing.Point(260, 72);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 9;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "TIC TAC TOE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+
+        private string num = "123456789";
+        private Random random = new Random();
+        private int rdNum = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ai()
+        {
+            string str = "";
+
+            rdNum = random.Next(0, num.Length);
+
+            str = num[rdNum].ToString();
+
+            switch (str)
+            {
+                case "1":
+                    b1.Text = "O";
+                    b1.Enabled = false;
+                    num = num.Replace("1", "");
+                    break;
+                case "2":
+                    b2.Text = "O";
+                    b2.Enabled = false;
+                    num = num.Replace("2", "");
+                    break;
+                case "3":
+                    b3.Text = "O";
+                    b3.Enabled = false;
+                    num = num.Replace("3", "");
+                    break;
+                case "4":
+                    b4.Text = "O";
+                    b4.Enabled = false;
+                    num = num.Replace("4", "");
+                    break;
+                case "5":
+                    b5.Text = "O";
+                    b5.Enabled = false;
+                    num = num.Replace("5", "");
+                    break;
+                case "6":
+                    b6.Text = "O";
+                    b6.Enabled = false;
+                    num = num.Replace("6", "");
+                    break;
+                case "7":
+                    b7.Text = "O";
+                    b7.Enabled = false;
+                    num = num.Replace("7", "");
+                    break;
+                case "8":
+                    b8.Text = "O";
+                    b8.Enabled = false;
+                    num = num.Replace("8", "");
+                    break;
+                case "9":
+                    b9.Text = "O";
+                    b9.Enabled = false;
+                    num = num.Replace("9", "");
+                    break;
+            }
+        }
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            b1.Enabled = true;
+            b2.Enabled = true;
+            b3.Enabled = true;
+            b4.Enabled = true;
+            b5.Enabled = true;
+            b6.Enabled = true;
+            b7.Enabled = true;
+            b8.Enabled = true;
+            b9.Enabled = true;
+
+            b1.Text = "";
+            b2.Text = "";
+            b3.Text = "";
+            b4.Text = "";
+            b5.Text = "";
+            b6.Text = "";
+            b7.Text = "";
+            b8.Text = "";
+            b9.Text = "";
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            b1.Text = "X";
+           
+            num = num.Replace("1", "");
+
+            b1.Enabled = false;
+
+            ai();
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            b2.Text = "X";
+
+            num = num.Replace("2", "");
+
+            b2.Enabled = false;
+
+            ai();
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            b3.Text = "X";
+
+            num = num.Replace("3", "");
+
+            b3.Enabled = false;
+
+            ai();
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            b4.Text = "X";
+
+            num = num.Replace("4", "");
+
+            b4.Enabled = false;
+
+            ai();
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            b5.Text = "X";
+
+            num = num.Replace("5", "");
+
+            b5.Enabled = false;
+
+            ai();
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            b6.Text = "X";
+
+            num = num.Replace("6", "");
+
+            b6.Enabled = false;
+
+            ai();
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            b7.Text = "X";
+
+            num = num.Replace("7", "");
+
+            b7.Enabled = false;
+
+            ai();
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            b8.Text = "X";
+
+            num = num.Replace("8", "");
+
+            b8.Enabled = false;
+
+            ai();
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            b9.Text = "X";
+
+            num = num.Replace("9", "");
+
+            b9.Enabled = false;
+
+            ai();
+        }
+    }
+}
